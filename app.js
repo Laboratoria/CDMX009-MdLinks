b1dd125700dc73e273fed831fad6946bbb9a22ff
shari12 <shariperez97@gmail.com>
2020-05-25 08:30:15 -0500

    array objets links

--- app.js
+++ app.js
@@ -1,46 +1,57 @@
-let fs = require('fs');
-let path = require('path');
-let url = require('url');
-let http = require('http');
-let https = require('https');
-let fetch = require('node-fetch');
+let fs = require("fs");
+let path = require("path");
+let url = require("url");
+let http = require("http");
+let https = require("https");
+let fetch = require("node-fetch");
 
-const mdLinks = {}
+const mdLinks = {};
 
+/* function option(){
+  if (index<0){
+    return console.log("help \n" 
+    + "para leer un archivo:\n" +
+    "debes ingresar la flag --read seguido de la ruta"
+    )
+  }
+
+}
+option() */
 function findFile() {
   //console.log("process ", process.argv);
-  let index = process.argv.indexOf('--file')
-  if(index < 0) return console.log("Necesitas usar la flag --file con un uri valido");
-  let uri = process.argv[index + 1]
+  let index = process.argv.indexOf("--read");
+  if (index < 0)
+    return console.log("Necesitas usar la flag --read con un uri valido");
+  let uri = process.argv[index + 1];
   console.log("uri ", uri);
-  let fileExt = path.extname(uri)
-  console.log("extension ", fileExt)
-  fileExt != '.md' ? console.log('Ingresa un archivo con extension .md') :
-  //console.log("Si es archivo .md") 
-  readFiles(uri)
+  let fileExt = path.extname(uri);
+  console.log("extension ", fileExt);
+  fileExt != ".md"
+    ? console.log("Ingresa un archivo con extension .md")
+    : //console.log("Si es archivo .md")
+      readFiles(uri);
 }
 
-function readFiles(uri){
-  let fileContent = fs.readFileSync(uri, 'utf-8');
+function readFiles(uri) {
+  let fileContent = fs.readFileSync(uri, "utf-8");
   //console.log("todo el texto aqui ", fileContent)
-  getLinks(fileContent, uri)
-  
+  getLinks(fileContent, uri);
 }
 
-function getLinks(fileContent,uri) {
-  let regexMarkdown= /\[(.+)\]\s(https?:.+?\S+)/gmi;//obtiene las etiquetas y links sin parentesis
+function getLinks(fileContent, uri) {
+  let regexMarkdown = /\[(.+)\]\s(https?:.+?\S+)/gim; //obtiene las etiquetas y links sin parentesis
   let regexURL = /https?:.+?\S+/gim;
-  let regexLabel =/\[(.+)\]\s/gim;//este regex trae todo lo que tenga []
-  let deleteBrackets = fileContent.replace(/[\(\)]/gim, " ");//aqui quitamos los parentesis y ponemos espacios
-  let matchMd = deleteBrackets.match(regexMarkdown)//trae los que coincidan con etiquetas y links
-  let matchURL = deleteBrackets.match(regexURL);//trae solo los links
- //console.log("etiquetas y links ",matchMd)
- //console.log("long l&l ", matchMd.length + " markdown")
- //console.log("match url ", matchURL);
- //console.log("log match url ", matchURL.length+ " links") 
+  let regexLabel = /\[(.+)\]\s/gim; //este regex trae todo lo que tenga []
+  let deleteBrackets = fileContent.replace(/[\(\)]/gim, " "); //aqui quitamos los parentesis y ponemos espacios
+  let matchMd = deleteBrackets.match(regexMarkdown); //trae los que coincidan con etiquetas y links
+  let matchURL = deleteBrackets.match(regexURL); //trae solo los links
+  //console.log("etiquetas y links ",matchMd)
+  //console.log("long l&l ", matchMd.length + " markdown")
+  //console.log("match url ", matchURL);
+  //console.log("log match url ", matchURL.length+ " links")
   //consultLinks(matchURL,regexURL,uri)
-  
-  textLinks(matchMd, regexMarkdown, uri,regexURL)
+
+  textLinks(matchMd, uri, regexURL, regexLabel);
 }
 
 /*function consultLinks(matchURL,regexURL,uri){
@@ -52,37 +63,48 @@ function getLinks(fileContent,uri) {
   }) 
 }*/
 
-  function textLinks(matchMd, regexMarkdown,uri,regexURL){
-    matchMd.forEach(element => {
-    if(element< 0) return console.log("No se encontaro coincidencias");
-       let markd = element.match(regexMarkdown)
-       //let markd = regexMarkdown.exec(element);
-       //console.log(markd)
-       markd === null ? console.log('sin coincidencias con etiquetas y urls') :
-       markd.forEach(labelLinks => {
-         let links = labelLinks.match(regexURL)
-         //console.log(links)
-         https.get(links, (res) => {
-          console.log('statusCode:', res.statusCode);
-          console.log('headers:', res.headers);
-        
-          res.on('data', (d) => {
-            process.stdout.write(d);
-          });
-        
-        }).on('error', (e) => {
-          console.error(e);
-        });      
-          
-       });
-     
-  });
-}
+function textLinks(matchMd, uri, regexURL, regexLabel) {
+  let arrayN = [];
+  matchMd.forEach((element) => {
+    if (element < 0) return console.log("No se encontaro coincidencias");
+    let links = element.match(regexURL);
+    let urls = element.match(regexLabel);
+    let newObjet = {
+      href: links,
+      label: urls,
+      path: uri,
+    };
+    arrayN.push(newObjet);
+    let getStatus = fetch(newObjet.href);
+         getStatus.then((res)=>{
+           return res.json();
+         }).then((json)=>{
+           console.log(json)
 
+         })
+    
+  });
+  //console.log(arrayN);
+  //console.log(arrayN.length);
+  
+
+  //prueba(arrayN)
+}
+/* function prueba(arrayN){
+  //console.log(arrayN)
+} */
+
+/* let getStatus = fetch('https://api.github.com/users/mitocode21');
+         getStatus.then((res)=>{
+           return res.json();
+         }).then((json)=>{
+           console.log(json)
+
+         }) */
 
 findFile();
 
-mdLinks.findFile=findFile;
-mdLinks.readFiles=readFiles;
+mdLinks.findFile = findFile;
+mdLinks.readFiles = readFiles;
 
 module.exports = mdLinks;
