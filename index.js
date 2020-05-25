919d82c19f47678bcec5fccff96e5098848c5780
danleonca <danleon022688@gmail.com>
2020-05-25 10:29:41 -0500

    add function allTheLinks

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 const fs = require('fs')
-const https = require('https')
 const fetch = require('node-fetch');
 const chalk = require('chalk')
-//function confirmFile (){
-let newFile = process.argv.indexOf('--file');
-let uri = process.argv[newFile + 1];
-let probe = uri.lastIndexOf(".");
-let typeFile = uri.slice(probe);
-//console.log(typeFile);
-if (typeFile === ".md") {
-    //console.log ("ahí vámos")
-    read(uri) //para leer el archivo
-        .then(content => findLinks(content) //para extraer los links
-        .then(linksReal=> getStatusLink(linksReal))
-           
-            ).catch(er=> console.log(er))
+
+function allTheLinks() {
+    let newFile = process.argv.indexOf('--file');
+    let uri = process.argv[newFile + 1];
+    let probe = uri.lastIndexOf(".");
+    let typeFile = uri.slice(probe);
+    //console.log(typeFile);
+    if (typeFile === ".md") {
+        //console.log ("ahí vámos")
+        read(uri) //para leer el archivo
+            .then(content => findLinks(content) //para extraer los links
+                .then(linksReal => getStatusLink(linksReal))
+
+            ).catch(er => console.log(er))
+    }
+    else { console.log("you need a file with an .md extention ") }
 }
-else { console.log("you need a file with an .md extention ") }
-//}
-//confirmFile();
+allTheLinks();
 
 
 function read(file) {
@@ -33,59 +33,27 @@ function read(file) {
 }
 
 const findLinks = (datos) => {
-    let joinArray= []
+    let joinArray = []
     return new Promise((resolve) => {
         let regularE = /(ftp|http|https):\/\/.*\)/gi;
-        let noStr= datos;
-        let str= noStr.toString();
+        let noStr = datos;
+        let str = noStr.toString();
         let myArray;
         while ((myArray = regularE.exec(str)) !== null) {
-            
-           // let joinData= myArray[0].replace(")", " ")
-           let joinData= {
-            link: myArray[0].replace(")", " "),
-            
-        };
-                
-            
-           
-         joinArray.push(joinData);
-          
-            }
-            //console.log(joinArray)
-            resolve(joinArray);
-            //let linksReal = joinArray.replace(")", " ");
-           
-        
+
+            // let joinData= myArray[0].replace(")", " ")
+            let joinData = {
+                link: myArray[0].replace(")", " "),
+
+            };
+            joinArray.push(joinData);
+        }
+        //console.log(joinArray)
+        resolve(joinArray);
 
     })
 }
 
-//function getStatusLink (array) {
-//
-//        const newArray = array.map( (element) => {
-//            
-//            return new Promise((resolve, reject)=> {
-//                
-//                fetch(element.Href)
-//                .then((res) => {
-//                    let object = {
-//                        Href: res.url,
-//                        Text: element.Text,
-//                        Status: res.status,
-//                        StatusText: res.statusText
-//                        };
-//                    resolve(console.log(href + Status));
-//                })
-//                .catch((err)=>{
-//                    reject("Error at element.href fetch", err)
-//                })
-//            })
-//        })
-//
-//    }
-
- 
 function getStatusLink(links) {
     let validLink = 0;
     let brokenLink = 0;
@@ -108,14 +76,14 @@ function getStatusLink(links) {
                 })
                 .then(() => {
                     if (links.length === validLink + brokenLink) {
-                return        console.log(chalk.magenta.bold(`Links totales: ${totalLinks = links.length}\n Links corectos:${validLink}\n Links erróneos: ${brokenLink}`))
+                        return console.log(chalk.magenta.bold(`Links totales: ${totalLinks = links.length}\n Links corectos:${validLink}\n Links erróneos: ${brokenLink}`))
                     }
                 })
                 .catch((err) => {
                     let objetFail = err.message
                     if (objetFail !== null) {
                         erros++;
-                       return  console.log(chalk.magenta.bold(`Total Links: ${totalLinks = links.length}\n   
+                        return console.log(chalk.magenta.bold(`Total Links: ${totalLinks = links.length}\n   
                                 Right Links :${validLink}\n 
                                 Wrong Links : ${brokenLink}\n
                                 Connection error : ${erros}   ${objetFail}`))
@@ -130,39 +98,11 @@ function getStatusLink(links) {
                         status: res.status,
                         statusText: res.statusText
                     };
-                   if(object.status===200) { console.log(chalk.green(`${object.href}  ${object.status}  OK `))}
-                   if (object.status!==200){console.log(chalk.red(`${object.href}  ${object.status}  BAD `))}
+                    if (object.status === 200) { console.log(chalk.green(`${object.href}  ${object.status}  OK `)) }
+                    if (object.status !== 200) { console.log(chalk.red(`${object.href}  ${object.status}  BAD `)) }
                 })
                 .catch(err => console.log(err.message))
         }
     })
 }
 
-//function workingLinks(links){
-//    https.get(links, function (res){
-//     
-//        console.log(links + 'statusCode:', res.statusCode);
-//    })
-//}
-
- //.then(linksReal=> linksReal.map((allLinks) => {
-            //    fetch(allLinks)
-            //    .then(res => console.log(res.status))
-            //            
-            //    .catch(err => console.error(err))
-            //}))
-
-            //function getStatusLink (array) {
-            //for (let i = 0; i < array.length; i++) {
-            //    fetch(array[i]).then((response) => {
-            //      if (response.status === 400 ) {
-            //          console.log(response.href + response.status  +  ' Error en el link')
-            //      }
-            //      else {  console.log(array.link  + response.status + " Link correcto") }
-            //      return response
-            //    }).catch((err)=>{
-            //        console.log("Error at fetch", err)
-            //    })
-            //  }
-            //}
-            //console.log( element.link + res.status)
