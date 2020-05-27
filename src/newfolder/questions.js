07ac8c16e6aa7bbe13379bd9ebeb87dbdfba3684
Mara Mulato <maramulato@MacBook-Mara.local>
2020-05-26 21:03:14 -0500

    trying inquirer

--- src/newfolder/questions.js
+++ src/newfolder/questions.js
@@ -1,2 +1,19 @@
 const inquirer = require('inquirer');
 
+module.exports = {
+  askFor: () => {
+    const questions = [
+      {
+        name: 'userfile',
+        type: 'input',
+        message: 'Please, type the relative path to your file:',
+        validate(value) {
+          if (value.length) {
+            return true;
+          }
+          return 'Please type a valid path';
+        },
+      },
+    ];
+  },
+};
