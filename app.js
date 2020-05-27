0dbb97e632f73b8e0b84f054305bb6439af93d95
usergithub <breen.78@hotmail.com>
2020-05-27 08:53:02 -0500

    add node-fetch

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 const fs = require('fs');
-const path = require('path'); 
+const path = require('path');
 const https = require('https');
+const fetch = require('node-fetch');
 let file;
 let string;
+let matches = [];
 
 //Find file
 const indexFile = () => {
@@ -14,28 +16,48 @@ const indexFile = () => {
 indexFile();
 
 //Read file
-  const readFile = () => {
+const readFile = () => {
   string = fs.readFileSync(file, 'utf8');
   return string;
 };
 
 //Find the md path file
-let pathFile = path.extname(file); 
-  if (pathFile === '.md'){
-    console.log('path:', pathFile);
-    readFile();
-  }
-  else {
-    console.log('path:', 'this is not a md file');
-  }
+let pathFile = path.extname(file);
+if (pathFile === '.md') {
+  console.log('path:', pathFile);
+  readFile();
+}
+else {
+  console.log('path:', 'this is not a md file');
+}
 //console.log('path:', pathFile) 
 
 //Find links
 const fileLinks = () => {
-  let matches = string.match(/\bhttps?:\/\/\S+/gi);
+  matches = string.match(/\bhttps?:\/\/\S+/gi);
   console.log('match:', matches);
 }
 fileLinks();
+
+//Validating links
+async function allLinks() { // PROHIBIDO USAR THEN
+let array = []
+  
+  matches.forEach(async element => {
+     let res = await fetch(element)
+     let obj = 
+     {url:res.url, 
+      status:res.status, 
+      text:res.statusText}
+      
+     console.log('obj', obj)
+     array.push(obj)
+    }) // esperar
+    console.log(array)
+  
+}
+allLinks();
+
 
 module.exports = indexFile;
 
