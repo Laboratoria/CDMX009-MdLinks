cfd1281d91bca7ab4588725d96e0fdaf49ddbb29
Kari2406 <karina.sremigio@gmail.com>
2020-05-28 22:42:11 -0500

    get links

--- index.js
+++ index.js
@@ -2,14 +2,16 @@
 
 let fs = require('fs');
 const path = require('path');
+const fetch = require('node-fetch');
 let file;
+let content;
 
 const readFile = () => {
     let fileFlag = process.argv.indexOf('--file');
     if (fileFlag < 0) return console.log('Ingresa --file seguido de una ruta válida')
     file = process.argv[fileFlag + 1];
-    let string = fs.readFileSync(file, 'utf-8');
-    return string;
+    content = fs.readFileSync(file, 'utf-8');
+    return content;
 }
 readFile();
 
@@ -24,11 +26,14 @@ const validateMD = () => {
         console.log('This isn\'t a .md file');
     }
 }
-console.log(validateMD());
-console.log(readFile());
 
 const getLinks = () => {
-
+    let regEx = (/https?:\S+\w/g);
+    let links = content.match(regEx)
+    return links
 }
+console.log(validateMD());
+console.log(readFile());
+console.log(getLinks());
 
 
