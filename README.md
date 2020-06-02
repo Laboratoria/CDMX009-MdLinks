42687c4a76c4a682553690a9da3282462227e0f7
danleonca <danleon022688@gmail.com>
2020-06-02 01:09:42 -0500

    addReadme

--- README.md
+++ README.md
@@ -1,4 +1,60 @@
 # Markdown Links "dan-mdlinks"
+"dan-mdlinks" es una libreria creada con java script para facilitar la extracción y verificación de los links que se encunetran en un archivo markdow (.md)
+ 
+ - Instalar la libreria via `npm install --global dan-mdlinks`
+
+### CLI (Command Line Interface - Interfaz de Línea de Comando)
+
+El ejecutable de la aplicación es de la de la siguiente
+manera a través de la terminal:
+
+`danleonca-mdlinks --file <path-to-file/directory> [options]`
+
+const danmdlinks = require("dan-mdlinks");
+
+Para acceder a las opciones puedes ejecutar 
+
+`danleonca-mdlinks --help`
+    --v         validate
+    --validate  validate
+    --s         stats
+    --stats     stats
+    --s/--stats && --v/--validate  stats & validate
+
+
+### Las opciones pueden ser:
+validar los links regresa un objeto donde puedes ver el path del archivo de donde se extrajo el link, el texto que tiene el link, el link, el estatus del link, y si se encuentra OK o Bad 
+
+    --v         validate
+    --validate  validate
+
+La opcion stats muestra estdísticas simples, donde puedes consultar cuantos links únicos se encuentran en cada uno de tus archivos, y cuantos de ellos se encuentran ok y cuantos Bad
+
+    --s         stats
+    --stats     stats
+
+De igual forma se pueden combinar ambas opciones para tener acceso a toda la información
+    --s/--stats && --v/--validate  stats & validate
+
+
+## La lógica que sigeu el proyecto se basa en  en el siguiente diagrama :
+
+![Diagrama](img/flowChart.png)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ## Preámbulo
 
--- utils/readFile.js
+++ utils/readFile.js
@@ -1,38 +1,53 @@
 const fs = require('fs')
 const path = require('path')
+const chalk = require('chalk')
 
-const read=(file)=>  {
-    let newFile = process.argv.indexOf('--file');
-    let uri = process.argv[newFile + 1];
-    let newPath;
-    if (!fs.lstatSync(file).isDirectory()) {
-        let probe = file.lastIndexOf(".");
-        let typeFile = uri.slice(probe);
-         return new Promise(function (resolve, reject) {
+const read = (file) => {
+    return new Promise(function (resolve, reject) {
+        let newFile = process.argv.indexOf('--file');
+        let uri = process.argv[newFile + 1];
+        
+        if (!fs.lstatSync(file).isDirectory()) {
+            let probe = file.lastIndexOf(".");
+            let typeFile = file.slice(probe);
             if (typeFile === ".md") {
-                fs.readFile(file, function (err, content) {
-                    if (err) {
-                        return reject(err)
-                    }
-                    resolve(content)
-                    console.log(content)
-        })
-    } else {console.log('you neeed a file with .md extention o you miss the flag --file befoere your path :) ')}
+            fs.readFile(file, function (err, content) {
+                if (err) {
+                    return reject(err)
+                }
+                resolve(content)
+                console.log(content)
+            })
+            } else {console.log('you neeed a file with .md extention o you miss the flag --file befoere your path :) ')}
+        }
+        else {
+            let direc = fs.readdirSync(uri)
+            direc.forEach(elemnt => {
+
+                let lastmd = elemnt.lastIndexOf(".");
+                let newF = elemnt.slice(lastmd);
+                if (newF === '.md') {
+                    let newPath = path.join(uri, elemnt)
+                    let expR = /\\/gi;
+                    let realPath = newPath.replace(expR, "\/");
+                        read(realPath)
+                   //resolve(read(realPath))
+                   console.log("archivos del directorio", realPath)
+                   
+
+                }
+                else {
+                    //return undefined 1.- Promesa este fuera del if
+                    // 2.- else  y hacer algo para no permitir que termine
+                    
+                    console.log(chalk.gray.bold("It is not a .md extention file ******" + "  " + elemnt))
+                }
+
+            })
+        }
     })
 }
-    else {
-            let direc= fs.readdirSync(file)
-                   direc.forEach(elemnt=>{
-                       let newF= path.extname(elemnt)
-                         //console.log(newF)
-                       if (newF === '.md') {
-                        newPath = path.join(uri, elemnt)
-                        let expR= /\\/gi;
-                        let realPath= newPath.replace(expR, "\/");
-                        console.log(realPath)
-                        read(realPath);
-                        }
-                   })    
-                }  
-}
+
+                
+
 module.exports= read; 
