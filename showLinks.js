a7e6f09fb679b6610292bda88350c7fd664add40
EdithOrt <eoc.diseno@gmail.com>
2020-06-02 13:53:03 -0500

    validated links and stats

--- showLinks.js
+++ showLinks.js
@@ -1,6 +1,5 @@
 const { validate } = require('./validate');
 function showLinks (file) {
-    let arrayLinks= [];
     let regEx= /\bhttps:\/\/([a-z0-9.a-z0-9\/]+)([-a-z0-9?=_&#\/]+)([.a-z0-9]+)/gi;
     let result = file.match(regEx);
     // console.log(result)
