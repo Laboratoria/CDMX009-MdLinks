4f59747449c408d85dd2bc602feaebd878d99613
vanyaxel <vanya_xel@live.com.mx>
2020-06-02 15:29:49 -0500

    funciones validate y stats

--- src/validateLinks.js
+++ src/validateLinks.js
@@ -36,34 +36,54 @@ function validateLinks(links, brokenLink, usefulLink) {
     console.log('useful    ok', usefulLink.length)
 }
 
-
+let brokenLink = [];
+let newArray = [];
 //funciones separadas de validacion y stats
-function statusLink(res, link) {
-    if (res.status !== 200) {
-        let brokenLink = (res); //como hago push al  array?
-        console.log(chalk.red(brokenLink.url + (chalk.bold.red('  x'))));
-        // console.log(typeof brokenLinks) //por que el tipo no es array y es string?
-        //return brokenLinks //se retorna?
-    }
-    if (res.status === 200) {
-        console.log(chalk.green(res.url + (chalk.bold.green('  ✔'))));
-    }
 
+function statusLink(res, totalLinks) {
+    newArray.push(res.url);
+    console.log('que hace esto', newArray)
+    if (totalLinks.length === newArray.length) {
+        console.log(newArray)
+    } else {
+        console.log("no se cumple")
+    }
+    //console.log(newArray.length)
+    /*  if (res.status !== 200) {
+         brokenLink.push(res.url);
+         // console.log(chalk.red(brokenLink.url + (chalk.bold.red('  x'))));
+         console.log(brokenLink);
+         return brokenLink;
+     }
+     if (res.status === 200) {
+         //console.log(chalk.green(res.url + (chalk.bold.green('  ✔'))));
+     }
+  */
 }
 
 function validateLinks(links) {
-    totals = links.length - 1;
-    //console.log(links); // este console log si plasma el array de links 
+    let total = [];
+    let totalLinks = links.length; // este console log si plasma el array de links 
     links.forEach(link => {
         //console.log(links)
-        fetch(link)
+        return fetch(link)
             .then((res) => {
-                statusLink(res, link)
-                //console.log(res)
+                if (res.status !== 200) {
+                    total.push(res);
+                    //console.log(chalk.red(res.url + (chalk.bold.red('  x'))));
+                    statusLink(res, total)
+                }
+                if (res.status === 200) {
+                    total.push(res);
+                    statusLink(res, total)
+                    //console.log(chalk.green(res.url + (chalk.bold.green('  ✔'))));
+                    //console.log(res)
+                }
             })
             .catch((e) => {
+                total.push(e);
                 //console.log('error------------', e)
-                statusLink(e, link)
+                statusLink(e, total)
             })
     }
     )
