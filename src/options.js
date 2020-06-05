569ea3da2d468bd7cc6073b0a0c9448de10454b2
ElizaFlores11 <60673917+ElizaFlores11@users.noreply.github.com>
2020-06-05 14:43:54 -0500

    Delete console

--- src/options.js
+++ src/options.js
@@ -28,8 +28,8 @@ const validate = (archive) => {
 const getLinksStats = (path) => new Promise((resolve) => {
   validate(path)
     .then((response) => {
-      const totalLinks = response.length;
-      const uniqueLinks = [...new Set(response.map((response) => response.href))].length;
+      let totalLinks = response.length;
+      let uniqueLinks = [...new Set(response.map((response) => response.href))].length;
       resolve(`Total : ${totalLinks} Unique: ${uniqueLinks}`);
     });
 });
