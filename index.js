2b5ab4763e89d19768eeac52270d6b3088db051e
akaren29 <akjassoj@outlook.com>
2020-06-05 18:16:13 -0500

    Se generá funcion para validar los links

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
+#!/usr/bin/env node
 const fs = require('fs');
-const path = require('path');
+let path = require('path');
 const marked = require('marked');
 const fetch = require('node-fetch');
-const colors= require('colors');
-
-let index = process.argv.indexOf("--file");
+const colors = require('colors');
 let flags = process.argv;
 
 function getFile(){
+    let index = process.argv.indexOf("--file");
     let uri = process.argv[index +1];
     let extension = path.extname(uri);
      if(index < 0){
@@ -17,12 +17,59 @@ function getFile(){
          console.log('Please enter file with extension markdown'.red);
          return false;
      }else{
-         return uri;
+         readFile(uri);
+         return uri;    
      }
 };
 
-function readFile (uri){
- let string = fs.readFileSync(uri,'utf-8');
- return string;
-}
+getFile();
 
+  function readFile(uri){
+   let fileMd = fs.readFileSync(uri,'utf-8');
+   searchLinks(fileMd, uri);
+   return fileMd; 
+  }
+
+
+  /* function searchLinks(fileMd, uri) {
+   let newFile = fileMd.replace(/[\(\)]/g, " ");
+   let space=" ";
+   let arrNewFile = newFile.split(space);
+   let arrLinks = arrNewFile.filter(text => text.includes('http'));
+   let objectLinks = {Links: arrLinks, File: uri};
+   console.log(objectLinks);
+   return objectLinks;
+   } */
+
+   function searchLinks(fileMd, uri){
+    let linksArr = []
+    let renderer = new marked.Renderer() 
+    renderer.link = ( href, file, text ) =>{
+         linksArr.push({
+            href: href,
+            title: text.slice(0,50),
+            file: uri
+        })
+    }
+    marked(fileMd, { renderer: renderer })
+    validateLinks(linksArr,uri);
+    return linksArr;
+}
+ 
+    function validateLinks(links, uri) {
+       const validateLinks= links.map(link => {
+       return fetch(link.href)
+        .then(res => {
+            if(res.status === 200){
+                console.log(`Href: ${res.url}  Status: ${res.status} OK ✓`.green);   
+            }else{
+                console.log(`Href: ${res.url}  Status: ${res.status} Failed ✕`.red);    
+            }
+        }).catch(error =>{
+            console.log(`Don´t find link: ${res.url}`);
+        })
+       })
+       return validateLinks;
+   } 
+
+   
