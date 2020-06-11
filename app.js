46bf4029f4357ffdca2e38a4046dd0260a2701a2
Dianyela Maldonado <dianyela.y.maldonado@gmail.com>
2020-06-11 12:40:30 -0500

    Changes in file app.js functions. i changed informations of functions until twenty five line. Now i begin to work in function of  check the links status

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
 const path = require('path');
 const fs = require('fs');
 
-// make sure it's a markdown file
+//make sure its a markdown file
 const isMarkDown = uri => path.extname(uri) === '.md' ? true : false;
 
 //The app must read a file
 const readFile = () => {
-    let index = process.argv.indexOf("--file"); //flag--> --file
-    if (index < 0) return console.log("no se encuentra el archivo");
-    let uri = process.argv[index + 1];
-    if (isMarkDown(uri)) {
-        let fileInformation = fs.readFileSync(uri, 'utf8');
-        showLinks(fileInformation);
+        let index = process.argv.indexOf("--file"); //flag--> --file
+        if (index < 0) return console.log("no se encuentra el archivo");
+        let uri = process.argv[index + 1];
+        if (isMarkDown(uri)) {
+            let fileInformation = fs.readFileSync(uri, 'utf8');
+            showLinks(fileInformation);
+        }
     }
-}
-
-//Finding links
+    //finding links
 const showLinks = (fileInformation) => {
+    console.log("all the information contained in the md file", fileInformation);
     let regEx = /\bhttps:\/\/([a-z0-9.a-z0-9\/]+)([-a-z0-9?=_&#\/]+)([.a-z0-9]+)/gi;
     let result = fileInformation.match(regEx);
-    console.log("All links", result);
+    console.log("result", result);
 }
+
+//check link status
+
+
 module.exports = {
     readFile,
+
 }
