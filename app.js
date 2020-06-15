a213561a703c05a4ebd55874f4d5b45fcea753c6
Dianyela Maldonado <dianyela.y.maldonado@gmail.com>
2020-06-15 16:36:52 -0500

    I install the colors library, so in  app.js i do changes in function checkLinkStatus, this for i can see the links brokens and the links functionals

--- app.js
+++ app.js
@@ -1,31 +1,71 @@
 const path = require('path');
 const fs = require('fs');
+const fetch = require('node-fetch');
+const colors = require('colors/safe');
 
 //make sure its a markdown file
 const isMarkDown = uri => path.extname(uri) === '.md' ? true : false;
 
 //The app must read a file
 const readFile = () => {
-        let index = process.argv.indexOf("--file"); //flag--> --file
-        if (index < 0) return console.log("no se encuentra el archivo");
-        let uri = process.argv[index + 1];
-        if (isMarkDown(uri)) {
-            let fileInformation = fs.readFileSync(uri, 'utf8');
-            showLinks(fileInformation);
-        }
+    let index = process.argv.indexOf("--file"); //flag--> --file
+    if (index < 0) return console.log("no se encuentra el archivo");
+    let uri = process.argv[index + 1];
+    if (isMarkDown(uri)) {
+        let fileInformation = fs.readFileSync(uri, 'utf8');
+        showLinks(fileInformation);
     }
-    //finding links
+}
+
+
+//finding links
 const showLinks = (fileInformation) => {
     console.log("all the information contained in the md file", fileInformation);
     let regEx = /\bhttps:\/\/([a-z0-9.a-z0-9\/]+)([-a-z0-9?=_&#\/]+)([.a-z0-9]+)/gi;
     let result = fileInformation.match(regEx);
     console.log("result", result);
+    checkLinkStatus(result);
 }
 
 //check link status
+const checkLinkStatus = (allLinks) => {
+    let callAllLinks = allLinks.length;
+    console.log(callAllLinks);
+    let promises = allLinks.map(link => fetch(link)
+        .then(res => {
+            let thrownAnswer = { //respuesta que me arroja
+                url: res.url,
+                status: res.status,
+                text: res.statusText
+            }
+            if (thrownAnswer.status === 200) {
+                console.log(colors.magenta(res.status), colors.cyan(res.statusText), ('url:', link))
+            } else {
+                console.log(colors.yellow(res.status), colors.green(res.statusText), ('url:', link))
+            }
+            return thrownAnswer
+        })
+        .catch(error => {
+            let bug = {
+                url: link,
+                status: 'error',
+                text: error.errno
+            }
+            if (bug.status === 'error') {
+                console.log(colors.yellow(bug.status), colors.green(bug.text), ('url:', link))
+            }
+            return bug
+        })
+    )
+    return promises;
+}
+
+
+
+
+
 
 
 module.exports = {
     readFile,
-
 }
