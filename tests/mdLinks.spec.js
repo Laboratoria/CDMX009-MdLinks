390d9ffec16f0d58f58af51d711df510904a7bf1
Oralia <oraliagarrido@gmail.com>
2020-06-19 08:57:25 -0500

    add test

--- tests/mdLinks.spec.js
+++ tests/mdLinks.spec.js
@@ -1,5 +1,7 @@
 const { readLinks } = require("../utils/extractLinks");
 const { validateLinks } = require("../utils/validateLinks");
+const { validateAndStats } = require("../utils/validateAndStats");
+const { statsLinks } = require("../utils/stats");
 
 const path = require("path");
 
@@ -8,7 +10,7 @@ describe("readLinks", () => {
     expect(typeof readLinks).toEqual("function");
   });
 
-  it("it should filter Md file", () => {
+  it("it should filter URL of Md file", () => {
     const input = path.join(__dirname, "./readme.md");
 
     const output = [
@@ -26,12 +28,42 @@ describe("validateLinks", () => {
     expect(typeof validateLinks).toEqual("function");
   });
   it("should return an object with link and text", () => {
-    let links = ["https://google.com https://nodejs.org/"];
     let result = {
       URL: "https://google.com",
       text: "Sirve",
     };
-    validateLinks(links);
     expect(typeof result).toBe("object");
+  });
+
+  /* it("it should filter unique and broken links", () => {
+    const input = path.join(__dirname, "./readme.md");
+
+    const output = [
+      "https://www.youtube.com/watch?v=tAmQZwMXvo",
+      "https://www.link3.dev",
+    ];
+
+    expect(readLinks(input)).toEqual(output);
+    expect(JSON.stringify(readLinks(input))).toBe(JSON.stringify(output));
+  });*/
+
+  /*it('validateLinks in file README.md should return an array with 54 elements', () => {
+
+      const input = path.join(__dirname, "./README.md");
+
+      expect(mdLink.findLinks()).toHaveLength(54);
+    });*/
+  //pasar array en la entrada
+});
+
+describe("statsLinks", () => {
+  test("Should be a function", () => {
+    expect(typeof statsLinks).toEqual("function");
+  });
+});
+
+describe("validateAndStats", () => {
+  it("Should be a function", () => {
+    expect(typeof validateAndStats).toEqual("function");
   });
 });
