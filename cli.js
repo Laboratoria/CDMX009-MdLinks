fd171a2d76f067892a8dc5b34e649439e3a1cb53
Sheiki1 <kirokomerteabesos@gmail.com>
2020-06-25 16:39:24 -0500

    last details

--- cli.js
+++ cli.js
@@ -1,6 +1,8 @@
 const { validate, validateStats} = require('./validate');
 const { getLinks, uniqueLinks,stats} = require('./stats');
 const fs = require("fs")
+const path = require('path');
+const marked = require('marked');
 //const path = require('path');
 
 const opts={
@@ -23,24 +25,30 @@ let comando = argv._[0]
 
 switch (comando) {
     case "validate":
-        
-        let arr =validate(links)
-        return arr
+        const showValidate = (links)=>{
+            let arr =validate(links)
+            return arr
+        }
+        showValidate()
     break;
     case "stats":
-
-        let arr1= uniqueLinks(links)
-        let arr2=stats(links)
-        console.log(arr1)
-        return arr2
-           
+        function showStats(links){
+            let arr1= uniqueLinks(links)
+            let arr2=stats(links)
+            console.log(arr1)
+            return arr2
+        }
+        showStats(links) 
     break;
     case "validateStats":
-        let arr3 =validateStats(links)
-        let arr4 = uniqueLinks(links)
-        return arr3 ,arr4
+        function showValidateStats(links){
+            let arr3 =validateStats(links)
+            let arr4 = uniqueLinks(links)
+            return arr3 ,arr4
+        }
+        showValidateStats(links)
     break;
     default:
-        console.log("no es valido ")
+        console.log("no es valido ingresa stats , validate o validateStats")
     break;
 }  
